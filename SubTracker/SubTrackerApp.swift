6d144ad4fa23beb042227a9839f98ef6f14867e6
chathuka18 <chathuka.20220975@iit.ac.lk>
2025-10-30 11:04:26 +0530

    Initial Commit

---
+++ SubTracker/SubTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubTrackerApp.swift
+//  SubTracker
+//
+//  Created by Chathuka Gamage on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct SubTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
